9feb4126e166940eb61a26d13d9276c38a698dca
oscar-javier <oscarjav99@unitec.edu>
2025-05-24 16:14:11 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,41 @@ import 'areas_geometricas.dart';
 import 'menu_helper.dart';
 
 void main() {
+ while (true) {
+    printMenuPrincipal();
+    String? opcion = stdin.readLineSync();
 
+    switch (opcion) {
+      case '1':
+        suma();
+        break;
+      case '2':
+        resta();
+        break;
+      case '3':
+        multiplicacion();
+        break;
+      case '4':
+        division();
+        break;
+      case '5':
+        numerosPrimosEnRango();
+        break;
+      case '6':
+        clasificarParesImpares();
+        break;
+      case '7':
+        fibonacci();
+        break;
+      case '8':
+        menuAreasGeometricas();
+        break;
+      case '9':
+        print("Gracias por usar la calculadora. ¡Hasta pronto!");
+        return;
+      default:
+        print("Opción inválida. Intente nuevamente.\n");
+    }
+  }
 }
 
