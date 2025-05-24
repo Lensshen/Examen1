e22dd6f2c5649f483ed0fd0afb72f142c86f3e9a
Leans <Leans_ortega@unitec.edu>
2025-05-24 15:56:38 -0600

    CUARTO

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'fibonacci.dart';
+import 'utilidades.dart';
+import 'menu_helper.dart';
 
 void main() {
-import 'dart:io';
-import 'operaciones_basicas.dart';
-import 'numeros_primos.dart';
-import 'pares_impares.dart';
-import 'fibonacci.dart';
-import 'areas_geometricas.dart';
-import 'menu_helper.dart';
-}
+  while (true) {
+    printMenuPrincipal();
+    String? opcion = stdin.readLineSync();
 
+    switch (opcion) {
+      case '1':
+        suma();
+        break;
+      case '2':
+        resta();
+        break;
+      case '3':
+        multiplicacion();
+        break;
+      case '4':
+        division();
+        break;
+      case '5':
+        numerosPrimosEnRango();
+        break;
+      case '6':
+        clasificarParesImpares();
+        break;
+      case '7':
+        fibonacci();
+        break;
+      case '8':
+        menuAreasGeometricas();
+        break;
+      case '9':
+        print("Gracias por usar la calculadora. ¡Hasta pronto!");
+        return;
+      default:
+        print("Opción inválida. Intente nuevamente.\n");
+    }
+  }
+}
